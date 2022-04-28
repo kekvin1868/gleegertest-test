2ca97f0acaf431818a269e2cedd9aa5bdb1057ac
Kevin Hartono <kevinhartono2810@gmail.com>
2022-04-28 18:56:53 +0700

    add header temp

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomeBase />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeBase from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomeBase,
   }
 }
 </script>
