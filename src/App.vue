60fee79f5ecbee4ec35575b0f0a1652dbbfd93b7
Kevin Hartono <kevinhartono2810@gmail.com>
2022-04-29 10:05:23 +0700

    finished header

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,78 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
+    <v-app-bar color="white" height="84px" app>
+      <div class="mx-4">
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
+          v-bind:src="require('../src/assets/sparrow-logo.png')"
+          class="pr-3 align-center"
+          max-width="150px"
+          max-height="64px"
         />
       </div>
 
+      <v-div fluid class="mx-4" align-content="center">
+        <v-div md="4" class="my-0 py-0">
+          <div>
+            <v-icon>mdi-bitcoin</v-icon>
+            <v-text class="ml-1 font-weight-medium">Bitcoin</v-text>
+          </div>
+        </v-div>
+        <v-div md="4" class="my-0 py-0">
+          <div class="ml-1">
+            <v-text class="font-weight-light text-h6">88,888</v-text>
+            <v-text class="font-weight-light text-caption">.88</v-text>
+          </div>
+        </v-div>
+      </v-div>
+
+      <v-div fluid class="mx-4 ml-6" align-content="center">
+        <v-div md="4" class="my-0 py-0">
+          <div>
+            <v-icon>mdi-ethereum</v-icon>
+            <v-text class="ml-1 font-weight-medium">Ethereum</v-text>
+          </div>
+        </v-div>
+        <v-div md="4" class="my-0 py-0">
+          <div class="ml-1">
+            <v-text class="font-weight-light text-h6">88,888</v-text>
+            <v-text class="font-weight-light text-caption">.88</v-text>
+          </div>
+        </v-div>
+      </v-div>
+
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-row align="center" justify="space-between" no-gutters>
+        <v-btn text rounded>
+          <v-text class="font-weight-medium text-caption">ConvertNOW</v-text>
+        </v-btn>
+
+        <v-btn text rounded>
+          <v-text class="font-weight-medium text-caption">Options</v-text>
+        </v-btn>
+
+        <v-btn text rounded>
+          <v-text class="font-weight-medium text-caption">Funds</v-text>
+        </v-btn>
+
+        <v-btn text rounded>
+          <v-text class="font-weight-medium text-caption">Support</v-text>
+        </v-btn>
+
+        <v-btn outlined rounded color="blue">
+          <v-text class="font-weight-medium text-caption">Settlement Price</v-text>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon dark>mdi-account</v-icon>
+        </v-btn>
+        
+        <v-divider class="profile-divider" vertical />
+
+        <v-btn icon>
+          <v-icon dark>mdi-earth</v-icon>
+        </v-btn>
+      </v-row>
     </v-app-bar>
 
     <v-main>
@@ -58,3 +96,9 @@ export default {
   }),
 };
 </script>
+
+<style>
+.profile-divider{
+  border-width: 1px !important;
+}
+</style>
